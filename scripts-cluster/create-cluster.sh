ac2a0023407862f9d1ccbb7c0fad756f52703639
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-05-09 12:44:28 +0530

    change

--- scripts-cluster/create-cluster.sh
+++ scripts-cluster/create-cluster.sh
@@ -1,2 +1,2 @@
 eksctl create cluster -f ./scripts-cluster/cluster-blueprint.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/aws/deploy.yaml --dry-run
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/aws/deploy.yaml 
