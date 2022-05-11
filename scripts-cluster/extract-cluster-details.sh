f5b36813565729891a7d974ce81dab043b91dc7a
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-05-11 12:40:49 +0530

    test

--- scripts-cluster/extract-cluster-details.sh
+++ scripts-cluster/extract-cluster-details.sh
@@ -1,3 +1,2 @@
 HOST_NAME=$(kubectl -n ingress-nginx get svc ingress-nginx-controller -o json | jq .status.loadBalancer.ingress[0].hostname)
-export HOST_NAME="$HOST_NAME"
-echo "${HOST_NAME}"
+echo "$HOST_NAME" | tr -d '"'
