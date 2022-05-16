5145bce96861a0c689026d4a6d8df37538b8a32e
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-05-16 23:52:34 +0530

    test

--- scripts-cluster/delete-cluster.sh
+++ scripts-cluster/delete-cluster.sh
@@ -1,1 +1,3 @@
+cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst '${APIM_EKS_CLUSTER_NAME}' | envsubst '${APIM_CLUSTER_REGION}' > ./scripts-cluster/cluster-blueprint.yaml
+cat ./scripts-cluster/cluster-blueprint.yaml
 eksctl delete cluster -f ./scripts-cluster/cluster-blueprint.yaml --wait
