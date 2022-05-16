9ad41b9caeaf12451d44f245779b30ae6ee20b56
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-05-16 21:50:47 +0530

    test

--- scripts-cluster/create-cluster.sh
+++ scripts-cluster/create-cluster.sh
@@ -1,3 +1,4 @@
+cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst ${APIM_EKS_CLUSTER_NAME} | envsubst ${APIM_CLUSTER_REGION}
 cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst ${APIM_EKS_CLUSTER_NAME} | envsubst ${APIM_CLUSTER_REGION} > ./scripts-cluster/cluster-blueprint.yaml
 eksctl create cluster -f ./scripts-cluster/cluster-blueprint.yaml
 aws s3 cp ~/.kube/config s3://apim-test-grid/profile-automation/kube-config
