152b575bd1ef026786ad7ce260b672e04d60b437
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-03 10:36:58 +0530

    test

--- scripts-cluster/create-cluster.sh
+++ scripts-cluster/create-cluster.sh
@@ -7,27 +7,27 @@ isEmpty () {
     fi;
 }
 
-isEmpty "${APIM_EKS_CLUSTER_NAME}";
+isEmpty "${EKS_CLUSTER_NAME}";
 flag=$?
 if [ $flag = 1 ];
-    then echo "APIM_EKS_CLUSTER_NAME environment variable is empty."; exit 1
+    then echo "EKS_CLUSTER_NAME environment variable is empty."; exit 1
 fi;
 
-isEmpty "${APIM_CLUSTER_REGION}";
+isEmpty "${EKS_CLUSTER_REGION}";
 flag=$?
 if [ $flag = 1 ];
-    then echo "APIM_CLUSTER_REGION environment variable is empty."; exit 1
+    then echo "EKS_CLUSTER_REGION environment variable is empty."; exit 1
 fi;
 
-isEmpty "${APIM_EKS_CLUSTER_NAME}";
+isEmpty "${EKS_CLUSTER_NAME}";
 flag=$?
 if [ $flag = 1 ];
-    then echo "APIM_EKS_CLUSTER_NAME environment variable is empty."; exit 1
+    then echo "EKS_CLUSTER_NAME environment variable is empty."; exit 1
 fi;
 
 
-cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst '${APIM_EKS_CLUSTER_NAME}' | envsubst '${APIM_CLUSTER_REGION}' > ./scripts-cluster/cluster-blueprint.yaml
-eksctl get cluster --region ${APIM_CLUSTER_REGION} -n ${APIM_EKS_CLUSTER_NAME} && echo "Cluster already exists" && exit 1
+cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst '${EKS_CLUSTER_NAME}' | envsubst '${EKS_CLUSTER_REGION}' > ./scripts-cluster/cluster-blueprint.yaml
+eksctl get cluster --region ${EKS_CLUSTER_REGION} -n ${EKS_CLUSTER_NAME} && echo "Cluster already exists" && exit 1
 eksctl create cluster -f ./scripts-cluster/cluster-blueprint.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/aws/deploy.yaml 
 
--- scripts-cluster/delete-cluster.sh
+++ scripts-cluster/delete-cluster.sh
@@ -1,3 +1,3 @@
-cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst '${APIM_EKS_CLUSTER_NAME}' | envsubst '${APIM_CLUSTER_REGION}' > ./scripts-cluster/cluster-blueprint.yaml
-eksctl get cluster --region ${APIM_CLUSTER_REGION} -n ${APIM_EKS_CLUSTER_NAME} ||  { echo 'Cluster does not exists.'; exit 1; }
+cat ./scripts-cluster/cluster-blueprint-template.yaml | envsubst '${EKS_CLUSTER_NAME}' | envsubst '${EKS_CLUSTER_REGION}' > ./scripts-cluster/cluster-blueprint.yaml
+eksctl get cluster --region ${EKS_CLUSTER_REGION} -n ${EKS_CLUSTER_NAME} ||  { echo 'Cluster does not exists.'; exit 1; }
 eksctl delete cluster -f ./scripts-cluster/cluster-blueprint.yaml --wait
