f080ed14c5f86b1749fa56116cc5253c77403bbb
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-07 13:12:12 +0530

    test

--- scripts-cluster/cleanup.sh
+++ scripts-cluster/cleanup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 productName=$1;
-sh ./scripts-cluster/"$productName"/cleanup.sh
+sh ./scripts-cluster/"$productName"/cleanup.sh && \ 
+    echo "Deleting RDS database." && \ 
+    aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} ; aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} || true
+
