f63ccfa310a7c1cf97b549cf6f74c2b901bd170d
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-08 09:39:30 +0530

    test

--- scripts-cluster/test.sh
+++ scripts-cluster/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 productName=$1;
 relativeFilePathTestFile=$2
-sh ./scripts-cluster/"$productName"/"$relativeFilePathTestFile"
+sh ./scripts-cluster/"$productName"/"test-$productName"/"$relativeFilePathTestFile"
