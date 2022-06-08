24c1051654e6f0c776d78cc160bb479aa3a19af2
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-08 09:37:58 +0530

    test

--- scripts-cluster/cleanup.sh
+++ scripts-cluster/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 productName=$1;
-sh ./scripts-cluster/"$productName"/cleanup.sh && \ 
-    echo "Deleting RDS database." && \ 
-    aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} ; aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} || true
+sh ./scripts-cluster/"$productName"/cleanup.sh || true
+echo "Deleting RDS database." || true
+aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} ; aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} || true
 
 echo "Scaling node group instances to zero."
 eksctl scale nodegroup --region ${EKS_CLUSTER_REGION} --cluster ${EKS_CLUSTER_NAME} --name ng-1 --nodes=0 || true
